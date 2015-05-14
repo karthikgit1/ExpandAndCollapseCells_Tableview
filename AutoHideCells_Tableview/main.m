a0d40ec0bb5fafa13de0e0ab34cb117f08f89872
karthikeyan <karthikeyan.karthik25@gmail.com>
2015-05-14 15:20:53 +0530

    Initial Commit

---
+++ AutoHideCells_Tableview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoHideCells_Tableview
+//
+//  Created by Vy Systems - iOS1 on 5/14/15.
+//  Copyright (c) 2015 Vy Systems - iOS1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
